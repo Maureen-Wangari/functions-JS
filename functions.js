4a6b7877502eadf2d06c6bd075f7c5aaf5000e12
Maureen-Njeri <maureenwangarinjeri@gmail.com>
2024-03-29 19:50:50 +0300

    add functions JavaScript

---
+++ functions.js
@@ -0,0 +1,31 @@
+//Sort and Revere order of a astring
+function arrange(){
+    let fruits = ["oranges", "Mango", "Apple", "Melon", "Pineapples"]
+    console.log(fruits.sort());
+    console.log(fruits.reverse());
+}
+
+//return positive or negative according to condition
+function SearchThrough(){
+let ages = ages.forEach(element =>{
+    if(element>0){
+        console.log("positive");
+    }
+    else if(element<0){
+        log("negative")
+    }
+    else{
+        console.log("zero");
+    }
+})
+}
+
+//multiply by two
+multiplication = [5, 4, 3, 2, 1]
+function multiplication(nums){
+    nums.forEach((nums=>{
+        console.log(nums*2);
+    }))
+}
+
+
